756e339b33ca34417a79e3f8f3f0a8e7e6026fb5
Chad Flores <chadwickflores@gmail.com>
2017-03-10 10:41:35 +0000

    setup routing and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :questions
+  root 'home#index'
+
+  resources :questions do
+    resources :answers
+  end 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
