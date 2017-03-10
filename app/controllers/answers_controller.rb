6530ba70e920dbbeca07836ab793886b84f3cde8
Chad Flores <chadwickflores@gmail.com>
2017-03-10 11:16:42 +0000

    questions setup and complete forum index

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,11 +4,14 @@ class AnswersController < ApplicationController
         @answer = @question.answers.build(answers_params)
         @answer.user = current_user
         @answer.save
+        
+        redirect_to questions_url
     end
     
     def destroy
         @question = Question.find(params[:question_id])
         @answer = @question.answers.find(params[:id]).destroy
+        
         redirect_to questions_url
     end
     
